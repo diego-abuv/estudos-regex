c3dd3cfda4ad3c4412eaf260784d65ebb3b9cba1
Diego A. Bueno Vitor <abuvitordiego.contato@gmail.com>
2026-03-12 20:18:26 -0300

    Melhorando busca por nomes completos

--- busca_nome.js
+++ busca_nome.js
@@ -2,7 +2,12 @@ const fs = require('fs')
 const bancoCsv = 'database.csv'
 const banco = fs.readFileSync(bancoCsv, "utf-8")
 
-const nome = /Cleberson/
-// regex simples para encontrar o nome especificado no banco de dados, armazena o conteúdo do array encontrado.
+const nome = /^([A-Za-zÀ-ÿ]+)(?:(\s[A-Za-zÀ-ÿ]+))+/gm
+// ^              -> Início da linha (comportamento ativado pela flag 'm').
+// ([A-Za-zÀ-ÿ]+) -> Grupo de captura do primeiro nome (letras e acentos).
+// (?: ... )+     -> Grupo de não-captura (?:) que agrupa os sobrenomes. O '+' exige um ou mais sobrenomes.
+// \s[A-Za-zÀ-ÿ]+ -> Estrutura do sobrenome: um espaço em branco (\s) seguido por letras.
+// Resumo: A regex busca nomes completos (Ex: "João Silva"), exigindo ao menos um sobrenome após o primeiro nome.
+
 const nomesEncontrados = banco.match(nome)
 console.log(nomesEncontrados)
