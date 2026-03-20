6f0413b06315f0f7b1ac5fda10137a008237fe59
Diego Bueno <abuvitordiego.contato@gmail.com>
2026-03-20 10:49:09 -0300

    Aprimorando validação de endereços: adicionando documentação da regex e implementando a filtragem de endereços inválidos

--- Validação-de-Endereços/valida-endereco.js
+++ Validação-de-Endereços/valida-endereco.js
@@ -3,8 +3,24 @@ const enderecosCsv = './Validação-de-Endereços/enderecos.csv'
 const enderecos = fs.readFileSync(enderecosCsv, "utf-8")
 
 const regexEndereco = /^(.*[A-Za-zÀ-ÿ])(, )(\d.*)\2(CEP (\d{5}-\d{3}))/gm
+/*
+    Explicação da regex:
+    - ^: Início da linha
+    - (.*[A-Za-zÀ-ÿ]): Captura o nome da rua (qualquer caractere seguido por uma letra)
+    - (, ): Captura a vírgula e o espaço
+    - (\d.*): Captura o número do endereço
+    - \2: Referência à segunda captura (a vírgula e o espaço)
+    - (CEP (\d{5}-\d{3})): Captura o CEP no formato 12345-678 (5 digitos, hífen, 3 dígitos)
+    - $: Fim da linha
+*/
 
+const linhas = enderecos.split('\n'); // Divide o conteúdo do arquivo em linhas
+
+// Encontra os endereços corretos usando a regex
 const enderecosCorretos = enderecos.match(regexEndereco)
 console.log('Endereços encontrados: ', enderecosCorretos)
 
-// a fazer - documentar regex e implementar retorno de endereços incorretos
+// Encontra os endereços inválidos usando filter e testa cada linha com a regex (se a linha não corresponder à regex, é considerada inválida)
+const invalidos = linhas.filter(linha => !regexEndereco.test(linha));
+console.log('Endereços inválidos: ', invalidos)
+
